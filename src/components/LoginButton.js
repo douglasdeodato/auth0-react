659f93695d8b7db4cab2ece2b95d3c7e4f4187f4
douglas <douglasdeodato@gmail.com>
2023-09-15 12:57:18 +0100

    fixing the error issue

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,15 +1,24 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
 
 const LoginButton = () => {
     const { loginWithRedirect, isAuthenticated } = useAuth0();
+    const navigate = useNavigate(); // Use useNavigate para redirecionar após o login
+
+    const handleLogin = () => {
+        loginWithRedirect({
+            // Redirecione para a URL desejada após o login bem-sucedido.
+            returnTo: '',
+        });
+    };
 
     return (
         !isAuthenticated && (
-            <button onClick={() => loginWithRedirect()}>
+            <button onClick={handleLogin}>
                 Sign In
             </button>
         )
     )
 }
 
-export default LoginButton
+export default LoginButton;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Importe o BrowserRouter
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
@@ -14,7 +15,9 @@ ReactDOM.render(
       clientId={clientId}
       redirectUri={window.location.origin}
     >
-      <App />
+      <Router> {/* Adicione o Router para envolver o aplicativo */}
+        <App />
+      </Router>
     </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
